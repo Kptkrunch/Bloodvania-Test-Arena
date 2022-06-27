eae37834eb326bc601ebdefdfc2780719c761f6b
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-06-27 18:49:50 -0500

    player movement now working with gravity and left/right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
-{   
+{
+    [SerializeField] float runSpeed = 10f;
 
     Vector2 moveInput;
+    Rigidbody2D playerRB2D;
 
     void Start()
     {
-        
+
+        playerRB2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+
+        Run();
+        FlipSprite();
     }
 
-    void OnMove(InputValue value) {
+    void OnMove(InputValue value)
+    {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+        // Debug.Log(moveInput.x);
+        // Debug.Log(moveInput.y);
     }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, playerRB2D.velocity.y);
+        Debug.Log(playerVelocity);
+        playerRB2D.velocity = playerVelocity;
+    }
+
+    void FlipSprite() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRB2D.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(-playerRB2D.velocity.x), 1.0f);
+        }
+    }
+
 }
